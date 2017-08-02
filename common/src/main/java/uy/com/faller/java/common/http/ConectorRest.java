c519a1b1a52f1855d610f6aff9cf6ed82dd3962a
Falucho <gfaller@antel.com.uy>
2017-08-02 16:35:06 -0300

    Se trabajo con el conector Rest

--- common/src/main/java/uy/com/faller/java/common/http/ConectorRest.java
+++ common/src/main/java/uy/com/faller/java/common/http/ConectorRest.java
@@ -9,6 +9,10 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import uy.com.faller.java.common.exception.CommonException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 public class ConectorRest {
@@ -16,24 +20,57 @@ public class ConectorRest {
   public static final int HTTP_OK = 200;
 
 
-  public static void invocar(List<BasicNameValuePair> urlParameters, String restURL) throws CommonException {
-    HttpPost post = new HttpPost(restURL);
-    try {
-      post.setEntity(new UrlEncodedFormEntity(urlParameters));
-      HttpClient client = new DefaultHttpClient();
-      HttpResponse response = client.execute(post);
-
-      int codigo = response.getStatusLine().getStatusCode();
-      if (codigo != HTTP_OK) {
-        throw new RuntimeException("ERROR  - Codigo devuelto :" + codigo);
-      }
-    } catch (Exception e) {
-      if (e instanceof CommonException) {
-        throw (CommonException) e;
-      }
-      throw new CommonException("ERROR ", e);
-    } finally {
-      post.releaseConnection();
+  public static void invocarOneWay(List<BasicNameValuePair> urlParameters, String restURL) throws CommonException {
+    int codigo = invocar(urlParameters, restURL).getStatusLine().getStatusCode();
+    if (codigo != HTTP_OK) {
+      throw new RuntimeException("ERROR  - Codigo devuelto :" + codigo);
     }
+
   }
+
+  public static HttpResponse invocar(List<BasicNameValuePair> urlParameters, String restURL) throws CommonException {
+    HttpPost post = new HttpPost(restURL);
+      try {
+          post.setEntity(new UrlEncodedFormEntity(urlParameters));
+          HttpClient client = new DefaultHttpClient();
+          HttpResponse response = client.execute(post);
+
+          int codigo = response.getStatusLine().getStatusCode();
+          if (codigo != HTTP_OK) {
+              throw new RuntimeException("ERROR  - Codigo devuelto :" + codigo);
+            }
+          return response;
+
+        } catch (Exception e) {
+          throw new CommonException("ERROR ", e);
+        } finally {
+          post.releaseConnection();
+        }
+    }
+
+  public static String getBody(HttpResponse response) throws CommonException {
+    String body = null;
+    StringBuilder stringBuilder = new StringBuilder();
+    BufferedReader bufferedReader = null;
+
+    try {
+      InputStream inputStream = response.getEntity().getContent();
+
+      if (inputStream != null) {
+        bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        char[] charBuffer = new char[128];
+        int bytesRead = -1;
+        while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+          stringBuilder.append(charBuffer, 0, bytesRead);
+        }
+      } else {
+        stringBuilder.append("");
+      }
+    } catch (IOException e) {
+      throw new CommonException("", e);
+    }
+    return stringBuilder.toString();
+  }
+
+
 }
---
+++ common/src/test/java/uy/com/faller/java/common/http/ConectorRestTest.java
@@ -0,0 +1,26 @@
+package uy.com.faller.java.common.http;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.message.BasicNameValuePair;
+import org.junit.Test;
+import uy.com.faller.java.common.exception.CommonException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConectorRestTest {
+
+  @Test
+  public void invocar() throws CommonException {
+
+    String url = "http://rest-service.guides.spring.io/greeting";
+    List<BasicNameValuePair> urlParameters = new ArrayList<>();
+
+
+    HttpResponse response = ConectorRest.invocar(urlParameters, url);
+    String body = ConectorRest.getBody(response);
+
+    System.out.println(body);
+  }
+
+}
